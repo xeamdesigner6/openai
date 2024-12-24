dff9a46d7c8d4d233343a156ef6a9244ab47e3fe
xeamdesigner6 <xeam.designer6@gmail.com>
2024-12-24 17:08:55 +0530

    chnaes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ConsolePage } from './pages/ConsolePage';
 import ScenarioForm from './pages/FormPage';
 import './App.scss';
+import { ConsolePageNew } from './pages/ConsolePageNew';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           {/* Define routes for different pages */}
           <Route path="/" element={<ConsolePage />} />
           <Route path="/form" element={<ScenarioForm />} />
+          <Route path="/console" element={<ConsolePageNew />} />
         </Routes>
       </Router>
     </div>
